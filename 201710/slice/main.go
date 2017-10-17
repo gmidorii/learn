4def52012b883068a9813071d5dfd69a62a33c60
midorigreen <stitow14@gmail.com>
2017-10-17 14:19:04 +0900

    Fix sample code

--- 201710/slice/main.go
+++ 201710/slice/main.go
@@ -6,25 +6,51 @@ import (
 )
 
 func main() {
-	sample()
-	sample1()
+	s := []int{}
+	for i := 0; i < 4; i++ {
+		start := time.Now()
+		switch i {
+		case 0:
+			s = sample()
+		case 1:
+			s = sample1()
+		case 2:
+			s = sample2()
+		case 3:
+			s = sample3()
+		default:
+			break
+		}
+		fmt.Printf("%d: %5d ns ", i, time.Now().Sub(start).Nanoseconds())
+		fmt.Println(s)
+	}
 }
 
-func sample() {
-	start := time.Now()
-	s := []int{1, 2, 3, 4, 5}
-	s = append(s, 6)
-	fmt.Println(s)
-	fmt.Printf("%d ns\n", time.Now().Sub(start).Nanoseconds())
+func sample() (s []int) {
+	s = []int{0, 1, 2, 3, 4}
+	return
 }
 
-func sample1() {
-	start := time.Now()
-	s := make([]int, 0)
-	for i := 1; i < 6; i++ {
+func sample1() (s []int) {
+	s = make([]int, 0)
+	for i := 0; i < 5; i++ {
 		s = append(s, i)
 	}
-	s = append(s, 6)
-	fmt.Println(s)
-	fmt.Printf("%d ns\n", time.Now().Sub(start).Nanoseconds())
+	return
+}
+
+func sample2() (s []int) {
+	s = make([]int, 0, 5)
+	for i := 0; i < 5; i++ {
+		s = append(s, i)
+	}
+	return
+}
+
+func sample3() (s []int) {
+	s = make([]int, 5, 5)
+	for i := 0; i < 5; i++ {
+		s[i] = i
+	}
+	return
 }
