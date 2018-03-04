f44f3343983606e98990b176428d7547cb5a4abc
tagasouichiro <stitow14@gmail.com>
2018-03-04 22:12:34 +0900

    Add Q3.1

--- 201803/go-system-programing/third/main.go
+++ 201803/go-system-programing/third/main.go
@@ -4,10 +4,14 @@ import (
 	"bytes"
 	"encoding/binary"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
 	endian()
+	copyFile()
 }
 
 func endian() {
@@ -16,4 +20,25 @@ func endian() {
 	var i int32
 	binary.Read(bytes.NewReader(data), binary.BigEndian, &i)
 	fmt.Printf("data: %d\n", i)
+}
+
+func copyFile() {
+	input, err := os.Open("./old.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer input.Close()
+
+	output, err := os.Create("./new.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer output.Close()
+
+	written, err := io.Copy(output, input)
+	if err != nil {
+		log.Println(err)
+	}
+
+	fmt.Printf("written count: %v \n", written)
 }
